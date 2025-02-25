b1fd8d13b19ddde92315ad082e356fe6ca42b36f
Elifnaz00 <demirelifnaz9@gmail.com>
2025-02-25 22:40:55 +0300

    js loop ödevi eklendi

---
+++ script.js
@@ -0,0 +1,20 @@
+
+//Collatz Dizisi 
+//-- i ye başlangıç değeri olarak verdiğim her değerde 1 ile sonlanıyor.
+
+var i = 13;
+while(i > 0 && i >= 2)
+{
+    console.log(i);
+    
+    if(i % 2 == 0)
+    {
+        i = i / 2; ;
+
+    }
+    else {
+        i = i*3+ 1;
+    }
+   
+    
+}
